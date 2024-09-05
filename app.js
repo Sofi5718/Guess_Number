41a0828d2eae6d807d7d3f828339ceea0fe290b6
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-09-05 10:34:13 +0200

    Binary Search gæt del 2

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ window.addEventListener("load", start);
 let randomNum;
 let guessNum = 1;
 
+let min = 1;
+let max = 100;
+
 function start() {
     document.querySelector("#start").addEventListener("click", () => {
         output(`is it ${randomNum}`);
+        document.querySelector("#start").disabled = true;
     });
     document.querySelector("#correct").addEventListener("click", correct);
     document.querySelector("#higher").addEventListener("click", higher);
@@ -16,25 +20,38 @@ function start() {
 const values = [...Array.from({ length: 100 }, (_, i) => i + 1)];
 
 function guess() {
-    return Math.floor(Math.random() * 100) + 1;
+    return Math.floor((min + max) / 2);
 }
 
 function correct() {
     output(`it took ${guessNum} guesses`);
+    document.querySelector("#higher").disabled = true;
+    document.querySelector("#lower").disabled = true;
 }
 
 function higher() {
     guessNum++;
-    randomNum = guess();
-    output(`Is it ${randomNum}`);
+    min = randomNum + 1;
+    checkOption();
 }
 
 function lower() {
     guessNum++;
-    randomNum = guess();
-    output(`Is it ${randomNum}`);
+    max = randomNum - 1;
+    checkOption();
 }
 
 function output(text) {
     document.querySelector("#guesses").insertAdjacentHTML("beforeend", `<li>${text}</li>`);
+}
+
+function checkOption() {
+    if (min === max) {
+        output(`there is only one option left ${min}`);
+        document.querySelector("#higher").disabled = true;
+        document.querySelector("#lower").disabled = true;
+    } else {
+        randomNum = guess();
+        output(`Is it ${randomNum}?`);
+    }
 }
